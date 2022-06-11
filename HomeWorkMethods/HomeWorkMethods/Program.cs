0ecb4a9e838f94de5530e58ef406b45ccfce1bda
ravanmaxmud <ravanim@code.edu.az>
2022-06-11 19:14:53 +0400

    Added HomeWork

---
+++ HomeWorkMethods/HomeWorkMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
